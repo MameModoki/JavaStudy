a16a1581b78e3e9857a873f74eaf1850c532e39d
MameModoki <omametubu0707@gmail.com>
2025-02-03 19:08:30 +0900

    SpringBoot入れた

---
+++ src/main/java/org/javastudy/controller/TaskController.java
@@ -0,0 +1,25 @@
+package org.javastudy.controller;
+
+import org.javastudy.model.Task;
+import org.javastudy.repository.TaskRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    private final TaskRepository taskRepository;
+
+    public TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+}
